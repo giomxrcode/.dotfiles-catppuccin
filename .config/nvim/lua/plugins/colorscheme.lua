c3cc74485c4c0feb21674a96c09c4e5058c4509e
giomxrcode <giomxr.code@gmail.com>
2024-10-04 15:47:24 -0400

    update: neovim lazylock.json and colorscheme plugin

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,12 @@
 return {
+  -- add catppuccin
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+  -- configure LazyVim to load catppuccin
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "catppuccin",
     },
   },
 }
